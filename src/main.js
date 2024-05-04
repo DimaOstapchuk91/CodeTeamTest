c8857b1d4e9e81d1326a30c39657a8b7a7bc3733
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 16:19:34 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 // import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css';
 
 import './js/reviews-slider.js';
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+// import Swiper from 'swiper/bundle';
+
+// // import styles bundle
+// import 'swiper/css/bundle';
+
+// // init Swiper:
+// const swiper = new Swiper(...);
+// import Swiper from 'swiper';
+// import { Navigation, Pagination } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+//   ...
+// });
