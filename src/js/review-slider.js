46a2d40508bcdae4245f7e45263e9980fb6e1dca
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 15:31:50 +0300

    bagfix

--- src/js/review-slider.js
+++ src/js/review-slider.js
@@ -1,12 +1,14 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
-  slidesPerView: 1,
+  sliderPerViev: 1,
   spaceBetween: 14,
   breakpoints: {
-    768: {
+    // when window width is >= 480px
+    798: {
       slidesPerView: 2,
       spaceBetween: 16,
     },
+    // when window width is >= 640px
     1280: {
       slidesPerView: 3,
       spaceBetween: 0,
@@ -17,6 +19,8 @@ const swiper = new Swiper('.swiper-container', {
     bulletClass: 'pagination__button',
     bulletActiveClass: 'pagination__button--active',
   },
+
+  // Navigation arrows
   navigation: {
     nextEl: '.carousel-button.next',
     prevEl: '.carousel-button.prev',
