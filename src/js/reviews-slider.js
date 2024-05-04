5fcbc095703dfa9c01ec55647a88abac498581e5
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 15:25:11 +0300

    fixed

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,19 +1,22 @@
-window.addEventListener('resize', function () {
-  if (window.innerWidth < 1279) {
-    // Ініціалізуємо Swiper для мобільних пристроїв
-    const swiper = new Swiper('.swiper-container', {
-      loop: true,
-      slidesPerView: 1,
-      spaceBetween: 16,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-        },
-      },
-    });
-  } else {
-    // Відключаємо Swiper для десктопних пристроїв
-    // Можливо, ви захочете видалити існуючий Swiper, якщо він уже ініційований
-    // наприклад, swiper.destroy()
-  }
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
 });
