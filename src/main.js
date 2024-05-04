b19e8720f84625a09e033475488a1f80882d2dea
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 16:10:36 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 // import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css';
 
 import './js/reviews-slider.js';
-import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
